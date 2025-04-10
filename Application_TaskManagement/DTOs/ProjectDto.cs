adda154b9c175d53584e0ee21fa5cc5f9f12d47a
Jovanovamartina <mjovanova@hotmail.com>
2025-04-10 11:17:06 +0200

    projectDto

---
+++ Application_TaskManagement/DTOs/ProjectDto.cs
@@ -0,0 +1,18 @@
+﻿
+
+using static Core_TaskManagement.Enums.EnumTypes;
+
+namespace Application_TaskManagement.DTOs
+{
+    public class ProjectDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public ProjectStatus Status { get; set; }
+        public ProjectPrioriry Priority { get; set; }
+        public ICollection<string>? AssignedTeamMembers { get; set; }
+    }
+}
