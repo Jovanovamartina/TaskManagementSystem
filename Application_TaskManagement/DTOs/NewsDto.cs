3e78af40c45e7ea8531edb9afe134c28ee3f2b26
Jovanovamartina <mjovanova@hotmail.com>
2025-08-19 09:32:15 +0200

    "changes"

--- Application_TaskManagement/DTOs/NewsDto.cs
+++ Application_TaskManagement/DTOs/NewsDto.cs
@@ -7,9 +7,7 @@ namespace Application_TaskManagement.DTOs
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public string? FilePath { get; set; }
         public int ProjectId { get; set; }
         public bool IsGeneral { get; set; }
-        public DateTime CreatedAt { get; set; }
     }
 }
--- Core_TaskManagement/Entities/News.cs
+++ Core_TaskManagement/Entities/News.cs
@@ -7,7 +7,6 @@ namespace Core_TaskManagement.Entities
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public string? FilePath { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public bool IsGeneral { get; set; } = true;
         public int? ProjectId { get; set; }
