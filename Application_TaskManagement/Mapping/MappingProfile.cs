8ade48f5e5efbcfca4cd5435a0d869fe42508415
Jovanovamartina <mjovanova@hotmail.com>
2025-08-22 10:53:40 +0200

    "update news updated"

--- Application_TaskManagement/Mapping/MappingProfile.cs
+++ Application_TaskManagement/Mapping/MappingProfile.cs
@@ -11,7 +11,15 @@ namespace Application_TaskManagement.Mapping
         public MappingProfile()
         {
             CreateMap<NewsCreateDto, News>().ReverseMap();
-            CreateMap<News, NewsDto>().ReverseMap();
+            //CreateMap<News, NewsDto>().ReverseMap();
+            CreateMap<News, NewsDto>()
+     .ReverseMap()
+     .ForMember(dest => dest.Id, opt => opt.Ignore())
+     .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+     .ForMember(dest => dest.CreatedByUserId, opt => opt.Ignore())
+     .ForAllMembers(opts =>
+         opts.Condition((src, dest, srcMember) => srcMember != null));
+
             CreateMap<RegisterDto, User>().ReverseMap();
             CreateMap<User, UserDto>().ReverseMap();
             CreateMap<UpdateUserDto, User>().ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
